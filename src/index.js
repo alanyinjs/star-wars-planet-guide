f908de0c558a275377d829afa7ed612b95eb2760
Alan Yin <yongqian.yin@gmail.com>
2018-06-28 12:03:12 +1000

    set up sass for styling

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import AppRouter from './routers/AppRouter';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 axios.defaults.baseURL = 'https://swapi.co/api';
 
 const app = (
