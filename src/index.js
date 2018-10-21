7d6e289b6f7657c96a1a12f53fc1108e6b692b80
Alan Yin <alanyinjs@gmail.com>
2018-10-21 14:28:24 +1100

    upgraded to react-script@2.0.4, optimised background image

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import AppRouter from './routers/AppRouter';
+import './App.scss';
 
 axios.defaults.baseURL = 'https://swapi.co/api';
 
